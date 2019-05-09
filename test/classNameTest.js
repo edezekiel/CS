830aa9aaf98ec5d1f30fce7878eb9a5a7da67cbb
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-09 15:57:05 -0500

    get all tests passing in browser

--- test/classNameTest.js
+++ test/classNameTest.js
@@ -8,5 +8,21 @@ describe('addClass', function() {
   }
 );
 
-  it('should not add a class which already exists')
+  it('should not add a class which already exists', function() {
+    var element = { className: 'exists' };
+
+    addClass(element, 'exists');
+
+    var numClasses = element.className.split(' ').length;
+    assert.equal(numClasses, 1);
+  });
+
+  it('should append new class after existing one', function() {
+    var element = { className: 'exists' };
+
+    addClass(element, 'new-class');
+
+    var classes = element.className.split(' ');
+    assert.equal(classes[1], 'new-class');
+  });
 });
