f736eb083dd89f4285d75fff5110144bf68ba744
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-16 16:50:02 -0500

    try to sort string before tabulating it

--- js/MaxCharacter.js
+++ js/MaxCharacter.js
@@ -1,5 +1,15 @@
 // solution to Silicon West Max Character problem
 
 const MaxCharacter = string => {
-  return 2
+  let splitString = string.split("")
+  maxCharacter = null
+  // iterate over each character in the split string
+  sortedString = splitString.sort()
+
+  // count how many times character appears in the string
+
+  // if count is higher, mark that character as highest
+
+  // return character with highest count
+  return sortedString
 }
