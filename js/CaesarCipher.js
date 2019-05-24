9d914c950ba11d9c0cdf447b4bdfd84a700e04eb
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-24 15:18:47 -0500

    midway through solution of caesar cipher

--- js/CaesarCipher.js
+++ js/CaesarCipher.js
@@ -1,1 +1,11 @@
-alert('hi there')
+const CaesarCipher = (phrase) => {
+  alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+  let cipher = {}
+  for (let i = 0; i < alphabet.length; i++) {
+    cipher[alphabet[i]] = i
+  }
+
+  for (word of phrase.split(" ")) {
+    console.log(word)
+  }
+}
