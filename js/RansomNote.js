8d68e9cf23c61e4f19932f2a14fcdafd3f85f60d
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-25 11:31:09 -0500

    code solution to ransom note problem

--- js/RansomNote.js
+++ js/RansomNote.js
@@ -1,19 +1,17 @@
 function RansomNote(string, magazine) {
   let createNote = true;
-  console.log(string)
-
   // for each word in string, is that word also a word in the magazine?
-
   // turn both into arrays
-
   let magazineArray = magazine.split(" ")
   let stringArray = string.split(" ")
 
   for (let i = 0; i < stringArray.length; i++) {
     if (!(magazineArray.includes(stringArray[i]))) {
       createNote = false
+    } else {
+      wordIndex = magazineArray.indexOf(stringArray[i])
+      magazineArray.splice(wordIndex, 1)
     }
   }
   return (createNote ? true : false)
-  // return true or false
 }
